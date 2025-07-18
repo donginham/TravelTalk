67dd7a4863ff196fd90c9bcb49557ce77fb8ec70
HDI <hdihdi1869@gmail.com>
2025-07-18 15:06:39 +0900

    Initial Commit

---
+++ TravelTalk/UserInfo/ProfileTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTableViewCell.swift
+//  TravelTalk
+//
+//  Created by HDI on 7/18/25.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
